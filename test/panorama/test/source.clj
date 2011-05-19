60c65b4f528ccfb1ee6bd955bf8490d850681e81
Matthew Maravillas <maravillas@gmail.com>
2011-05-18 23:42:46 -0400

    Update source tests

--- test/panorama/test/source.clj
+++ test/panorama/test/source.clj
@@ -3,54 +3,42 @@
         lamina.core
         [lazytest.describe :only [describe it testing]]))
 
-(describe "Source creation"
-  (it "includes an update function"
-    (let [source (make-source identity)]
-      (= (:fn source) identity)))
-  (it "initializes a channel"
-    (let [source (make-source identity)
-          ch (:channel source)
-          val {:test true}]
-      (enqueue ch val)
-      (= @(:state source)
-         val)))
-  (it "does not consume from the original channel"
-    (let [source (make-source identity)
-          ch (:channel source)
-          val {:test true}]
-      (enqueue ch val)
-      (= (count (channel-seq (:channel source)))
-         1)))
-  (it "specifies an update period"
-    (let [source (make-source identity 10)]
-      (= (:period source) 10000))))
+(describe "Converting names to ids"
+  (it "replaces spaces"
+    (= (name->id "a name")
+       "a-name"))
+  (it "lowercases"
+    (= (name->id "CASE")
+       "case"))
+  (it "replaces tabs"
+    (= (name->id "a\ttab")
+       "a-tab"))
+  (it "replaces hashes"
+    (= (name->id "we're#1!")
+       "we're-1!")))
 
+(describe "Updating a state ref"
+  (it "combines all updates into the ref"
+    (let [state (ref {})]
+      (alter-state state {:foo 1} {:bar 2 :baz 3})
+      (= @state
+         {:foo 1 :bar 2 :baz 3})))
+  (it "overwrites stale state"
+    (let [state (ref {:foo 10})]
+      (alter-state state {:foo 1} {:bar 2 :baz 3})
+      (= @state
+         {:foo 1 :bar 2 :baz 3})))
+  (it "preserves state with no updates"
+    (let [state (ref {:quux 10})]
+      (alter-state state {:foo 1} {:bar 2 :baz 3})
+      (= @state
+         {:foo 1 :bar 2 :baz 3 :quux 10}))))
 
-(describe "Updating sources"
-  (testing "of type server-status-4"
-    (it "applies a status of 'down' when there are no messages from the server"
-      (let [ch (channel)]
-        (enqueue ch {:foo "bar"})
-        (= (:status (update-server-status ch {:status "up"}))
-           "down")))
-    (it "applies the status provided by the server"
-      (let [ch (channel)]
-        (enqueue ch {:status "up"})
-        (= (:status (update-server-status ch {:status "down"}))
-           "up")))))
-
-(describe "Creating sources from config entries"
-  (it "creates a server-status source"
-    (map #(every? % [:fn :channel :state])
-         (config-entry->sources {:type :server-status
-                                 :ids ["id"]
-                                 :names ["name"]})
-            [:fn :channel :state]))
-  (it "creates server-status-4 sources"
-    (map #(every? % [:fn :channel :state])
-         (config-entry->sources {:type :server-status-4
-                                 :ids ["id1 id2 id3 id4"]
-                                 :names ["name1 name2 name3 name4"]}))))
-
-
-
+(describe "Recieving from a state channel"
+  (it "updates the state"
+    (let [state (ref {})
+          ch (channel)]
+      (fork-state-channel ch state)
+      (enqueue ch {:foo 1})
+      (= @state
+         {:foo 1}))))
